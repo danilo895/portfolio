911722fa5909274c29d0c212391ec405e1b6a5a7
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-19 10:59:25 +0100

    hero works

--- portfoliodl/src/app/components/hero/hero.component.ts
+++ portfoliodl/src/app/components/hero/hero.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   imports: [],
+  standalone: true,
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
