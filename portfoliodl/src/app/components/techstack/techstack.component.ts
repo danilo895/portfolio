71f1cc80d1d5b84e3963251bcd8392cc2a31bb45
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-19 10:15:57 +0100

    added components

---
+++ portfoliodl/src/app/components/techstack/techstack.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-techstack',
+  imports: [],
+  templateUrl: './techstack.component.html',
+  styleUrl: './techstack.component.scss'
+})
+export class TechstackComponent {
+
+}
