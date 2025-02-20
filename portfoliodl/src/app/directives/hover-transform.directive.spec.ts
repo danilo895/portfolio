7588d633b7d03995b71fffa65832144ac5f9c5eb
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-20 14:54:16 +0100

    little improvements

--- portfoliodl/src/app/directives/hover-transform.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HoverTransformDirective } from './hover-transform.directive';
-
-describe('HoverTransformDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HoverTransformDirective();
-    expect(directive).toBeTruthy();
-  });
-});
