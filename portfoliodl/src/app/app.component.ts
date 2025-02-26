81138b489598719ceeb00ef0e21b6b75f66b100d
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-26 13:20:37 +0100

    testimonial cont

--- portfoliodl/src/app/app.component.ts
+++ portfoliodl/src/app/app.component.ts
@@ -5,12 +5,20 @@ import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { TechstackComponent } from './components/techstack/techstack.component'; 
 import { ProjectsComponent } from './components/projects/projects.component';
-
+import { TestimonialsComponent } from './components/testimonials/testimonials.component'; // ✅ Import der neuen Komponente
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, TechstackComponent, ProjectsComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    TechstackComponent,
+    ProjectsComponent,
+    TestimonialsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- portfoliodl/src/app/components/testimonials/testimonials.component.ts
+++ portfoliodl/src/app/components/testimonials/testimonials.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-testimonials',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './testimonials.component.html',
   styleUrl: './testimonials.component.scss'
 })
-export class TestimonialsComponent {
-
-}
+export class TestimonialsComponent { }
