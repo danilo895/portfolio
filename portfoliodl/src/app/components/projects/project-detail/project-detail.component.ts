5001c462406d301defdbeca2a96664e14f6a9b37
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-26 09:36:27 +0100

    next button works

--- portfoliodl/src/app/components/projects/project-detail/project-detail.component.ts
+++ portfoliodl/src/app/components/projects/project-detail/project-detail.component.ts
@@ -1,28 +1,27 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Project } from '../../../interfaces/project.model';
+import { projects } from '../../../projects.data';
 
 @Component({
   selector: 'app-project-detail',
   standalone: true,
-  imports: [
-    CommonModule
-
-  ],
+  imports: [CommonModule],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.scss'
 })
 export class ProjectDetailComponent {
-
   @Input() project: Project = {} as Project;
-  @Output() closeDialog= new EventEmitter<void>();
-
-
-
-
-
+  @Output() closeDialog = new EventEmitter<void>();
+  @Output() projectChange = new EventEmitter<Project>();
   public closeProjectDetail() {
     this.closeDialog.emit();
   }
 
+  public nextProject() {
+    const currentIndex = projects.findIndex(p => p.id === this.project.id);
+    const nextIndex = (currentIndex + 1) % projects.length;
+    this.project = projects[nextIndex]; 
+    this.projectChange.emit(this.project); 
+  }
 }
