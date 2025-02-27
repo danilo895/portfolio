95f28fffe5bd0bf165fd7104b5e5151eca40b820
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-27 15:28:23 +0100

    hover effects contactsection

--- portfoliodl/src/app/components/contact/contact.component.ts
+++ portfoliodl/src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.setupHoverEffect('.form-group-name', ['.hr-1', '.hr-2'], '#3355FF');
+    this.setupHoverEffect('.form-group-email', ['.hr-2', '.hr-3'], '#3355FF');
+    this.setupHoverEffect('.form-group-message', ['.hr-3', '.hr-4'], '#3355FF');
+  }
+
+  setupHoverEffect(triggerSelector: string, targetSelectors: string[], color: string) {
+    const triggerElement = this.el.nativeElement.querySelector(triggerSelector);
+    if (triggerElement) {
+      triggerElement.addEventListener('mouseenter', () => {
+        targetSelectors.forEach(selector => {
+          const hrElement = this.el.nativeElement.querySelector(selector);
+          if (hrElement) {
+            this.renderer.setStyle(hrElement, 'backgroundColor', color);
+          }
+        });
+      });
+
+      triggerElement.addEventListener('mouseleave', () => {
+        targetSelectors.forEach(selector => {
+          const hrElement = this.el.nativeElement.querySelector(selector);
+          if (hrElement) {
+            const defaultColor = selector === '.hr-1' ? 'transparent' : '#F7C518';
+            this.renderer.setStyle(hrElement, 'backgroundColor', defaultColor);
+          }
+        });
+      });
+    }
+  }
 }
