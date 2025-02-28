c3e7a1e39f1eb5d50a28f534ebbc6b3ef83904fe
Daniel Loeffler <loefflerdaniel@web.de>
2025-02-28 12:16:53 +0100

    hover effects contacts scss

--- portfoliodl/src/app/app.component.ts
+++ portfoliodl/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { TechstackComponent } from './components/techstack/techstack.component'; 
 import { ProjectsComponent } from './components/projects/projects.component';
-import { TestimonialsComponent } from './components/testimonials/testimonials.component'; // ✅ Import der neuen Komponente
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
