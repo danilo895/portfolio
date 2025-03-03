c5a09af7bcc011570bdd660d125fba684739c5c7
Daniel Loeffler <loefflerdaniel@web.de>
2025-03-03 12:23:41 +0100

    translated testimonials work

--- portfoliodl/src/app/components/testimonials/testimonials.component.ts
+++ portfoliodl/src/app/components/testimonials/testimonials.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './testimonials.component.html',
   styleUrl: './testimonials.component.scss'
 })
