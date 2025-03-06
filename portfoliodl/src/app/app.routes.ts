3344afceaddb562f54378fa821f6e5090a3cef89
Daniel Loeffler <loefflerdaniel@web.de>
2025-03-06 21:23:38 +0100

    routing legal notice works

--- portfoliodl/src/app/app.routes.ts
+++ portfoliodl/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ImprintComponent } from './pages/imprint/imprint.component';
 
 export const appRoutes: Routes = [
-  { path: '', component: HomeComponent }  // Startseite
+{ path: '', component: HomeComponent },
+{ path: 'imprint', component: ImprintComponent } 
 ];
--- portfoliodl/src/app/pages/imprint/imprint.component.ts
+++ portfoliodl/src/app/pages/imprint/imprint.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
-export class ImprintComponent {
-
-}
+export class ImprintComponent {}
