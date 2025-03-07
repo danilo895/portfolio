9e77ab07dcab9554e01811a2ffccddcc453f4db2
Daniel Loeffler <loefflerdaniel@web.de>
2025-03-07 22:30:41 +0100

    smooth scroll router interaction

--- portfoliodl/src/app/shared/footer/footer.component.ts
+++ portfoliodl/src/app/shared/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -9,4 +9,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor(private router: Router) {}
+
+  navigateToImprint() {
+    this.router.navigate(['/imprint']).then(() => {
+      setTimeout(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }, 100);
+    });
+  }
+}
