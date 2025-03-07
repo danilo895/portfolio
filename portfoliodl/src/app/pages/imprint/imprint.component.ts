e6708daf0e149e4044fc2344896dfe22a0625f5c
Daniel Loeffler <loefflerdaniel@web.de>
2025-03-07 10:44:43 +0100

    translation imprint

--- portfoliodl/src/app/pages/imprint/imprint.component.ts
+++ portfoliodl/src/app/pages/imprint/imprint.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AppTranslateService } from '../../services/translate.service';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslateModule], 
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
-export class ImprintComponent {}
+export class ImprintComponent {
+  constructor(public translateService: AppTranslateService, private translate: TranslateService) {}
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+}
